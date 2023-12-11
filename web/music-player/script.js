10e57de70842c1d8baa057e9b498f11d8c9f0d25
llllllll-l <vincent.werelius@gmail.com>
2023-12-11 15:58:52 +0100

    js beginning (want spotify integration)

---
+++ web/music-player/script.js
@@ -0,0 +1,13 @@
+const image = document.getElementById('cover');
+const title = document.getElementById('music-title');
+const artist = document.getElementById('music-artist');
+const currentTime = document.getElementById('current-time');
+const duration = document.getElementById('duration');
+const progress = document.getElementById('progress');
+const playerProgress = document.getElementById('player-progress');
+const prevBtn = document.getElementById('prev');
+const platBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+const background = document.getElementById('bg-img');
+
+const music = new Audio();
